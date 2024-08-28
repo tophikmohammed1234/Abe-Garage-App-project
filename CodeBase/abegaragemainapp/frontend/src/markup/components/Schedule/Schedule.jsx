1cef33feaedfce0fe833b654f7fad75a882c6089
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-28 21:18:33 +0300

    home

---
+++ CodeBase/abegaragemainapp/frontend/src/markup/components/Schedule/Schedule.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Schedule = () => {
+  return (
+    <>
+      <section class="cta-section">
+        <div class="auto-container">
+          <div class="wrapper-box">
+            <div class="left-column">
+              <h3>Schedule Your Appointment Today</h3>
+              <div class="text">
+                Your Automotive Repair & Maintenance Service Specialist
+              </div>
+            </div>
+            <div class="right-column">
+              <div class="phone">1800.456.7890</div>
+              <div class="btn">
+                <a href="#" class="theme-btn btn-style-one">
+                  <span>Appointment</span>
+                  <i class="flaticon-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Schedule;
