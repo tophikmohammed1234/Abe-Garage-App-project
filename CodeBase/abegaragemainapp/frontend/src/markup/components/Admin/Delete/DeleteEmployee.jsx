b952c646f1ebe103673ad07ff58bbcb0e69483cf
yabiball <yabibalhunie@gmail.com>
2024-09-05 19:50:28 +0300

    spinner

--- CodeBase/abegaragemainapp/frontend/src/markup/components/Admin/Delete/DeleteEmployee.jsx
+++ CodeBase/abegaragemainapp/frontend/src/markup/components/Admin/Delete/DeleteEmployee.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import employeeService from "../../../../services/employee.service";
 import { useAuth } from "../../../../Context/AuthContext";
+import BarLoader from "react-spinners/BarLoader";
 import "./DeleteEmployee.css"; // Assuming you create a CSS file for styles
+
 
 const DeleteEmployee = () => {
 	const { id } = useParams();
@@ -13,6 +15,7 @@ const DeleteEmployee = () => {
 	const [employeeData, setEmployeeData] = useState(null);
 	const [deletedEmployee, setDeletedEmployee] = useState(null);
 	const [showModal, setShowModal] = useState(false); // To control modal visibility
+  const [isLoading, setIsLoading] = useState("");
 
 	// Fetch employee info before deletion
 	useEffect(() => {
@@ -37,14 +40,18 @@ const DeleteEmployee = () => {
 	};
 
 	const confirmDelete = async () => {
+		setIsLoading("loading")
 		try {
 			if (employeeData) {
 				const data = await employeeService.deleteEmployee(token, id);
 				if (data.success) {
+		setIsLoading("success");
+
 					setDeletedEmployee(data.data);
 					setTimeout(() => {
 						navigate("/admin/employees");
-					}, 5000);
+
+					}, 7000);
 				}
 			}
 		} catch (error) {
@@ -55,99 +62,108 @@ const DeleteEmployee = () => {
 	};
 
 	return (
-		<section className="p-5 container">
-			<h1 className="my-3 text-center">Delete Employee</h1>
+    <section className="p-5 container">
+      <h1 className="my-3 text-center">Delete Employee</h1>
 
-			{employeeData && (
-				<table>
-					<thead>
-						<tr>
-							<th>Attribute</th>
-							<th>Value</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>First Name</td>
-							<td>{employeeData.employee_first_name}</td>
-						</tr>
-						<tr>
-							<td>Last Name</td>
-							<td>{employeeData.employee_last_name}</td>
-						</tr>
-						<tr>
-							<td>Email</td>
-							<td>{employeeData.employee_email}</td>
-						</tr>
-						<tr>
-							<td>Phone</td>
-							<td>{employeeData.employee_phone}</td>
-						</tr>
-						<tr>
-							<td>Company Role ID</td>
-							<td>{employeeData.company_role_id}</td>
-						</tr>
-					</tbody>
-				</table>
-			)}
+      {employeeData && (
+        <table>
+          <thead>
+            <tr>
+              <th>Attribute</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>First Name</td>
+              <td>{employeeData.employee_first_name}</td>
+            </tr>
+            <tr>
+              <td>Last Name</td>
+              <td>{employeeData.employee_last_name}</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{employeeData.employee_email}</td>
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>{employeeData.employee_phone}</td>
+            </tr>
+            <tr>
+              <td>Company Role ID</td>
+              <td>{employeeData.company_role_id}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
 
-			<div className="btn-container text-center d-flex justify-content-center gap-3 my-3">
-				<button className="cancel-button">
-					<Link to={`/admin/employees`}>Cancel Delete</Link>
-				</button>
+      <div className="btn-container text-center d-flex justify-content-center gap-3 my-3">
+        <button className="cancel-button">
+          <Link to={`/admin/employees`}>Cancel Delete</Link>
+        </button>
 
-				<button className="delete-button" onClick={handleDelete}>
-					Delete Employee
-				</button>
-			</div>
+        <button className="delete-button" onClick={handleDelete}>
+          Delete Employee
+        </button>
+      </div>
 
-			{showModal && (
-				<div className="modal">
-					<div className="modal-content w-50">
-						<h3>Confirm Deletion</h3>
-						<p>
-							Are you sure you want to delete{" "}
-							<strong>
-								{employeeData.employee_first_name}{" "}
-								{employeeData.employee_last_name}
-							</strong>
-							?
-						</p>
-						<button className="confirm-button" onClick={confirmDelete}>
-							Yes, Delete
-						</button>
-						<button
-							className="cancel-button"
-							onClick={() => setShowModal(false)}
-						>
-							Cancel
-						</button>
-					</div>
-				</div>
-			)}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content w-50">
+            <h3>Confirm Deletion</h3>
+            <p>
+              Are you sure you want to delete{" "}
+              <strong>
+                {employeeData.employee_first_name}{" "}
+                {employeeData.employee_last_name}
+              </strong>
+              ?
+            </p>
+            <button
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "45px"
+              }}
+              className="confirm-button"
+              onClick={confirmDelete}
+            >
+              {isLoading==="loading" ? <BarLoader color="#f9c305" /> :isLoading === "success" ? "deleted success": "Yes, Delete"}
+            </button>
+            <button
+              className="cancel-button"
+              onClick={() => setShowModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
 
-			{deletedEmployee && (
-				<div className="deleted-info">
-					<h2>Deleted Employee Info</h2>
-					<p>
-						<strong>First Name:</strong> {deletedEmployee.employee_first_name}
-					</p>
-					<p>
-						<strong>Last Name:</strong> {deletedEmployee.employee_last_name}
-					</p>
-					<p>
-						<strong>Email:</strong> {deletedEmployee.employee_email}
-					</p>
-					<p>
-						<strong>Phone:</strong> {deletedEmployee.employee_phone}
-					</p>
-					<p>
-						<strong>Company Role ID:</strong> {deletedEmployee.company_role_id}
-					</p>
-				</div>
-			)}
-		</section>
-	);
+      {deletedEmployee && (
+        <div className="deleted-info">
+          <h2>Deleted Employee Info</h2>
+          <p>
+            <strong>First Name:</strong> {deletedEmployee.employee_first_name}
+          </p>
+          <p>
+            <strong>Last Name:</strong> {deletedEmployee.employee_last_name}
+          </p>
+          <p>
+            <strong>Email:</strong> {deletedEmployee.employee_email}
+          </p>
+          <p>
+            <strong>Phone:</strong> {deletedEmployee.employee_phone}
+          </p>
+          <p>
+            <strong>Company Role ID:</strong> {deletedEmployee.company_role_id}
+          </p>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default DeleteEmployee;
--- CodeBase/abegaragemainapp/frontend/src/markup/components/Admin/UpdateEmployeeForm/EmployeeUpdate.jsx
+++ CodeBase/abegaragemainapp/frontend/src/markup/components/Admin/UpdateEmployeeForm/EmployeeUpdate.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import employeeService from "../../../../services/employee.service";
+import BarLoader from "react-spinners/BarLoader";
 
 const EmployeeUpdate = ({
   show,
@@ -9,7 +10,6 @@ const EmployeeUpdate = ({
   loggedInEmployeeToken,
 }) => {
   const [employee_email, setEmail] = useState(selectedEmployee.employee_email);
-  //inetialize by defaul value
   const [employee_first_name, setFirstName] = useState(
     selectedEmployee.employee_first_name
   );
@@ -19,114 +19,99 @@ const EmployeeUpdate = ({
   const [employee_phone, setPhoneNumber] = useState(
     selectedEmployee.employee_phone
   );
-  const [employee_password, setPassword] = useState();
+  const [employee_password, setPassword] = useState("");
   const [active_employee, setActive_employee] = useState(
     selectedEmployee.active_employee
   );
   const [company_role_id, setCompany_role_id] = useState(
     selectedEmployee.company_role_id
   );
-  const [employee_id, setEmployee_id] = useState(selectedEmployee.employee_id);
+  const [employee_id] = useState(selectedEmployee.employee_id);
 
-  // Errors
   const [emailError, setEmailError] = useState("");
   const [firstNameRequired, setFirstNameRequired] = useState("");
   const [lastNameRequired, setLastNameRequired] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [phoneError, setPhoneError] = useState("");
   const [roleError, setRoleError] = useState("");
-  const [success, setSuccess] = useState(false);
   const [serverError, setServerError] = useState("");
-  const [UseOldPassword, setUseOldPassword] = useState(false);
+  const [success, setSuccess] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSaveChanges = (e) => {
-    // Prevent the default behavior of the form
     e.preventDefault();
 
-    // Handle client side validations
-    let valid = true; // Flag
-    // First name is required
+    let valid = true;
+    // Validate fields
     if (!employee_first_name) {
       setFirstNameRequired("First name is required");
       valid = false;
     } else {
       setFirstNameRequired("");
     }
-    // Last name is required
+
     if (!employee_last_name) {
       setLastNameRequired("Last name is required");
       valid = false;
     } else {
       setLastNameRequired("");
     }
-    // Phone number is required
+
     if (!employee_phone) {
       setPhoneError("Phone number is required");
       valid = false;
     } else {
       setPhoneError("");
     }
-    //Role is required
+
     if (!company_role_id) {
       setRoleError("Role is required");
       valid = false;
     } else {
       setRoleError("");
     }
-    // Email is required
-    if (!employee_email) {
-      setEmailError("Email is required");
-      valid = false;
-    } else if (!employee_email.includes("@")) {
+
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!employee_email || !emailRegex.test(employee_email)) {
       setEmailError("Invalid email format");
+      valid = false;
     } else {
-      const regex = /^\S+@\S+\.\S+$/;
-      if (!regex.test(employee_email)) {
-        setEmailError("Invalid email format");
-        valid = false;
-      } else {
-        setEmailError("");
-      }
+      setEmailError("");
     }
-    // Password has to be at least 6 characters long
-    if (!employee_password || employee_password.length < 6) {
+
+    if (!isChecked && (!employee_password || employee_password.length < 6)) {
       setPasswordError("Password must be at least 6 characters long");
       valid = false;
     } else {
       setPasswordError("");
     }
-    if (isChecked) {
-      setPassword(selectedEmployee.employee_password_hashed);
-    }
-    // If the form is not valid, do not submit
+    
+    
     if (!valid) {
       return;
     }
+    setIsLoading(true);
     const formData = {
       employee_email,
       employee_first_name,
       employee_last_name,
       employee_phone,
-      employee_password,
+      employee_password: isChecked
+        ? selectedEmployee.employee_password_hashed
+        : employee_password,
       active_employee,
       company_role_id,
       employee_id,
     };
 
-    // Pass the form data to the service
-    const updatedEmployee = employeeService.updateEmployee(
-      formData,
-      loggedInEmployeeToken
-    );
-    updatedEmployee
+    employeeService
+      .updateEmployee(formData, loggedInEmployeeToken)
       .then((response) => response.json())
       .then((data) => {
-        // If Error is returned from the API server, set the error message
         if (data.error) {
           setServerError(data.error);
         } else {
-          // Handle successful response
           setSuccess(true);
           setServerError("");
           setTimeout(() => {
@@ -134,30 +119,15 @@ const EmployeeUpdate = ({
           }, 1000);
         }
       })
-      // Handle Catch
       .catch((error) => {
         const resMessage =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
+          error.response?.data?.message || error.message || error.toString();
         setServerError(resMessage);
       });
-
-    // Close the modal
-    console.log(employee_password);
-    handleClose();
   };
 
   return (
-    <Modal
-      show={show}
-      onHide={handleClose}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
+    <Modal show={show} onHide={handleClose} size="md" centered>
       <Modal.Header closeButton>
         <Modal.Title>Edit Employee</Modal.Title>
       </Modal.Header>
@@ -171,7 +141,7 @@ const EmployeeUpdate = ({
               <Form.Label>First Name</Form.Label>
               <Form.Control
                 type="text"
-                defaultValue={selectedEmployee.employee_first_name}
+                value={employee_first_name}
                 onChange={(e) => setFirstName(e.target.value)}
                 style={{
                   width: "70%",
@@ -179,6 +149,7 @@ const EmployeeUpdate = ({
                 }}
               />
             </Form.Group>
+
             <Form.Group
               className="mb-3 d-flex justify-content-between"
               controlId="formLastName"
@@ -186,35 +157,31 @@ const EmployeeUpdate = ({
               <Form.Label>Last Name</Form.Label>
               <Form.Control
                 type="text"
+                value={employee_last_name}
+                onChange={(e) => setLastName(e.target.value)}
                 style={{
                   width: "70%",
                   ...(lastNameRequired ? { border: "1px solid red" } : {}),
                 }}
-                defaultValue={selectedEmployee.employee_last_name}
-                onChange={(e) => setLastName(e.target.value)}
-                // Add onChange handlers as needed
               />
             </Form.Group>
-            {/* justifyContent space-between */}
+
             <Form.Group
               className="mb-3 d-flex justify-content-between"
               controlId="formEmail"
             >
-              <Form.Label>
-                <p>Email address</p>
-              </Form.Label>
+              <Form.Label>Email address</Form.Label>
               <Form.Control
-                // alling to the end
+                type="email"
+                value={employee_email}
+                onChange={(e) => setEmail(e.target.value)}
                 style={{
                   width: "70%",
                   ...(emailError ? { border: "1px solid red" } : {}),
                 }}
-                type="email"
-                defaultValue={selectedEmployee.employee_email}
-                onChange={(e) => setEmail(e.target.value)}
-                // Add onChange handlers as needed
               />
             </Form.Group>
+
             <Form.Group
               className="mb-3 d-flex justify-content-between"
               controlId="formPhone"
@@ -222,54 +189,79 @@ const EmployeeUpdate = ({
               <Form.Label>Phone</Form.Label>
               <Form.Control
                 type="text"
+                value={employee_phone}
+                onChange={(e) => setPhoneNumber(e.target.value)}
                 style={{
                   width: "70%",
                   ...(phoneError ? { border: "1px solid red" } : {}),
                 }}
-                defaultValue={selectedEmployee.employee_phone}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                // Add onChange handlers as needed
               />
             </Form.Group>
-            <Form.Group className="mb-3 d-flex  " controlId="formRole">
+
+            <Form.Group
+              className="mb-3 d-flex justify-content-between"
+              controlId="formRole"
+            >
+              <Form.Label>Role</Form.Label>
               <Form.Select
-                style={{ border: "1px solid #ced4da" }}
-                aria-lable="Default select example"
                 value={company_role_id}
                 onChange={(e) => setCompany_role_id(e.target.value)}
+                style={{
+                  width: "70%",
+                  border: "1px solid #ced4da",
+                  borderRadius: "5px",
+                }}
               >
-                <option>Select The Company Role</option>
+                <option value="">Select The Company Role</option>
                 <option value={1}>Employee</option>
                 <option value={2}>Manager</option>
                 <option value={3}>Admin</option>
               </Form.Select>
             </Form.Group>
 
-            {/* Add more form fields as necessary */}
-            <Form.Group className="mb-3" controlId="formRole">
-              <Form.Label>Password</Form.Label>
-              <Form.Check
-                type="checkbox"
-                checked={isChecked}
-                label="Use old password"
-                onChange={() => setIsChecked(!isChecked)}
-              />
+            <Form.Group className="mb-3" controlId="formPassword">
+              <div className="d-flex justify-content-between">
+                <Form.Label>Password</Form.Label>
+                <Form.Check
+                  type="checkbox"
+                  checked={isChecked}
+                  label="Use old password"
+                  onChange={() => setIsChecked(!isChecked)}
+                />
+              </div>
               <Form.Control
-                type="text"
+                style={{
+                  width: "70%",
+                  ...(passwordError ? { border: "1px solid red" } : {}),
+                }}
+                type="password"
                 disabled={isChecked}
                 onChange={(e) => setPassword(e.target.value)}
-                // Add onChange handlers as needed
+                value={
+                  isChecked
+                    ? selectedEmployee.employee_password_hashed
+                    : employee_password
+                }
               />
             </Form.Group>
           </Form>
         )}
+        {serverError && <p style={{ color: "red" }}>{serverError}</p>}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={handleSaveChanges}>
-          Save Changes
+        <Button
+          style={{ height: "40px" }}
+          variant="primary"
+          onClick={handleSaveChanges}
+        >
+          {isLoading ? (
+            <BarLoader color="#f9c305" height={5} />
+          ) : (
+            "Save Changes"
+          )}
         </Button>
       </Modal.Footer>
     </Modal>
