9bb63cb1ba4f77ff057dff8246e27f385b9b94e5
eyo-as <eyosiyastumiso7@gmail.com>
2024-09-10 16:54:43 +0300

    get all customers

--- CodeBase/abegaragemainapp/backend/controllers/customer.controller.js
+++ CodeBase/abegaragemainapp/backend/controllers/customer.controller.js
@@ -1,45 +1,68 @@
 const customerService = require("../services/customer.service");
 
 async function addCustomer(req, res) {
-	try {
-		const {
-			customer_first_name,
-			customer_last_name,
-			customer_email,
-			customer_phone_number,
-			active_customer_status,
-			customer_hash,
-			customer_added_date,
-		} = req.body;
+  try {
+    const {
+      customer_first_name,
+      customer_last_name,
+      customer_email,
+      customer_phone_number,
+      active_customer_status,
+      customer_hash,
+      customer_added_date,
+    } = req.body;
 
-		if (
-			!customer_first_name ||
-			!customer_last_name ||
-			!customer_email ||
-			!customer_phone_number ||
-			!active_customer_status ||
-			!customer_hash ||
-			!customer_added_date
-		) {
-			return res.status(400).json({
-				error: "Bad Request",
-				message: "Please provide all required fields",
-			});
-		}
+    if (
+      !customer_first_name ||
+      !customer_last_name ||
+      !customer_email ||
+      !customer_phone_number ||
+      !active_customer_status ||
+      !customer_hash ||
+      !customer_added_date
+    ) {
+      return res.status(400).json({
+        error: "Bad Request",
+        message: "Please provide all required fields",
+      });
+    }
 
-		await customerService.addCustomer(req.body);
+    await customerService.addCustomer(req.body);
 
-		res.status(201).json({
-			message: "Customer created successfully",
-			success: true,
-		});
-	} catch (error) {
-		console.error("Error:", error);
-		res.status(500).json({
-			error: "Internal Server Error",
-			message: "An unexpected error occurred.",
-		});
-	}
+    res.status(201).json({
+      message: "Customer created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+      message: "An unexpected error occurred.",
+    });
+  }
 }
 
-module.exports = { addCustomer };
+// a function to get all customers
+async function getAllCustomers(req, res) {
+  try {
+    const customers = await customerService.getAllCustomers();
+
+    return res.status(200).json({
+      message: "Customers retrieved successfully",
+      success: true,
+      data: customers,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+      message: "An unexpected error occurred.",
+    });
+  }
+}
+
+// export all the functions
+module.exports = {
+  addCustomer,
+  getAllCustomers,
+};
--- CodeBase/abegaragemainapp/backend/routes/customer.routes.js
+++ CodeBase/abegaragemainapp/backend/routes/customer.routes.js
@@ -4,5 +4,7 @@ const customerController = require("../controllers/customer.controller");
 
 // Add a new customer
 router.post("/api/customer", customerController.addCustomer);
+// Get all customers
+router.get("/api/customers", customerController.getAllCustomers);
 
 module.exports = router;
