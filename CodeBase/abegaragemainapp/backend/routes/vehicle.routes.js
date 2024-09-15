2907760dda5207f735ddd970a0d9838d21b376e6
yabiball <yabibalhunie@gmail.com>
2024-09-16 00:22:33 +0300

    getVehicleById backend

--- CodeBase/abegaragemainapp/backend/routes/vehicle.routes.js
+++ CodeBase/abegaragemainapp/backend/routes/vehicle.routes.js
@@ -4,5 +4,6 @@ const vehicleController = require("../controllers/vehicle.controller");
 
 // Add a new vehicle
 router.post("/api/vehicle", vehicleController.addVehicle);
+router.get("/api/vehicle/:id", vehicleController.getVehicleById);
 
 module.exports = router;
