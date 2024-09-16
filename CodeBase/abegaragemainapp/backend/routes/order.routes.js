4d4cb0d41fa87a09b74de1eb200737acd142bf3a
Eibrahim Belayneh <160432177+Abuabdellahh@users.noreply.github.com>
2024-09-17 00:11:08 +0300

    Update order.routes.js

--- CodeBase/abegaragemainapp/backend/routes/order.routes.js
+++ CodeBase/abegaragemainapp/backend/routes/order.routes.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const orderController = require("../controllers/orderController");
-const authMiddleware = require("../middleware/authMiddleware");
 
-
-// delete the order
-router.delete(
-  "/api/order/:id",
-  authMiddleware.verifyToken,
-  authMiddleware.isAdmin,
-  orderController.deleteOrder
-);
-
-module.exports = router;
